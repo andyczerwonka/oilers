a8b669a72b5061b16e290d92cce7a061df11eb5d
Andy Czerwonka <andy.czerwonka@gmail.com>
2016-10-29 18:21:40 -0600

    data frame with all bet stats

--- sandbox.R
+++ sandbox.R
@@ -10,7 +10,7 @@ players <- subset(
   (X.LastName == "McDavid" & X.FirstName == "Connor") | (X.LastName == "Gaudreau" & X.FirstName == "Johnny")
   )
 names(players) <- c("Lastname", "Firstname", "Games", "Points")
-sortedPlayers <- players[order(players$Lastname, decreasing=TRUE),]
+players <- players[order(players$Lastname, decreasing=TRUE),]
 
 #team data
 teamstats <- read.csv("./data/teamstats.csv")
@@ -19,19 +19,22 @@ teams <- subset(
   X.Team.Name == "Oilers" | X.Team.Name == "Flames"
 )
 names(teams) <- c("City", "Team", "Games", "Points", "Goals")
-sortedTeams <- teams[order(teams$Team, decreasing=TRUE),]
+teams <- teams[order(teams$Team, decreasing=TRUE),]
 
 
 # create a new table with Andy & Adam rows
-betFrame <- data.frame(
+totalPoints <- teams[, 4] + teams[, 5] + players[, 4]
+gamesMissed <- teams[, 3] - players[, 3]
+betStats <- data.frame(
   Who = c("Andy","Adam"),
-  Team  = sortedTeams[, 2],
-  Team.Games = sortedTeams[, 3],
-  Team.Points = sortedTeams[, 4],
-  Team.Goals = sortedTeams[, 5],
-  Player  = sortedPlayers[, 1],
-  Player.Points = sortedPlayers[, 4],
-  Games.Missed = sortedTeams[, 3] - sortedPlayers[, 3]
+  Team  = teams[, 2],
+  Team.Games = teams[, 3],
+  Team.Points = teams[, 4],
+  Team.Goals = teams[, 5],
+  Player  = players[, 1],
+  Player.Points = players[, 4],
+  Games.Missed = gamesMissed,
+  Total = totalPoints
   )
 
 
