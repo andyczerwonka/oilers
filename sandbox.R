ba34394264a9cc5c1f932bb9e3a3137cd0310e42
Andy Czerwonka <andy.czerwonka@gmail.com>
2016-12-05 09:06:16 -0700

    changed to remove the player portion, Gaudreu has missed 10 games.

--- sandbox.R
+++ sandbox.R
@@ -6,46 +6,26 @@ library(curl)
 h <- new_handle()
 handle_setheaders(h, "Authorization" = "Basic YW5keWN6ZXJ3b25rYTpwb2xvbmlh")
 
-#player data
-players <-
-  read.csv(
-    curl(
-      "https://www.mysportsfeeds.com/api/feed/pull/nhl/2016-2017-regular/cumulative_player_stats.csv?playerstats=Pts",
-      handle = h
-    )
-  )
-players <- subset(
-  players[, c(3, 4, 18, 19)],
-  (X.LastName == "McDavid" &
-     X.FirstName == "Connor") |
-    (X.LastName == "Gaudreau" & X.FirstName == "Johnny")
-)
-players <- players[order(players$X.LastName, decreasing = TRUE), ]
-
 #team data
 teams <-
-  read.csv(
-    curl(
-      "https://www.mysportsfeeds.com/api/feed/pull/nhl/2016-2017/conference_team_standings.csv",
-      handle = h
+    read.csv(
+        curl(
+            "https://www.mysportsfeeds.com/api/feed/pull/nhl/2016-2017/conference_team_standings.csv",
+            handle = h
+        )
     )
-  )
 teams <- subset(teams[, c(5, 6, 8, 9, 10, 12, 13, 25)],
                 X.Team.Name == "Oilers" | X.Team.Name == "Flames")
-teams <- teams[order(teams$X.Team.Name, decreasing = TRUE), ]
+teams <- teams[order(teams$X.Team.Name, decreasing = TRUE),]
 
 # create a new table with Andy & Adam rows
-totalPoints <- teams[, 7] + teams[, 8] + players[, 4]
-gamesMissed <- teams[, 3] - players[, 3]
+totalPoints <- teams[, 7] + teams[, 8]
 betStats <- data.frame(
-  Who = c("Andy", "Adam"),
-  Team  = teams[, 2],
-  Team.Games = teams[, 3],
-  Team.Record = teamRecord(teams[, 4], teams[, 5], teams[, 6]),
-  Team.Points = teams[, 7],
-  Team.Goals = teams[, 8],
-  Player  = players[, 1],
-  Player.Points = players[, 4],
-  Games.Missed = gamesMissed,
-  Total = totalPoints
+    Who = c("Andy", "Adam"),
+    Team  = teams[, 2],
+    Team.Games = teams[, 3],
+    Team.Record = teamRecord(teams[, 4], teams[, 5], teams[, 6]),
+    Team.Points = teams[, 7],
+    Team.Goals = teams[, 8],
+    Total = totalPoints
 )
